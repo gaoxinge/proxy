4f7efde4a1651ce164ca66a92be084788b517354
高辛格 <xg.gao@tianrang-inc.com>
2018-10-16 17:31:00 +0800

    http_proxy.go

--- http_proxy.go
+++ http_proxy.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 	"io"
+	"log"
 )
 
 type HttpProxy struct{
@@ -26,32 +27,32 @@ func NewConnect() *Connect {
 	return &Connect{}
 }
 
-func (connect *Connect) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Received request %s %s %s\n", r.Method, r.Host, r.RemoteAddr)
-
-	if r.Method != "CONNECT" {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Write([]byte("This is a http tunnel proxy, only CONNECTED method is allowed"))
-		return
-	}
-
-	host := r.URL.Host
-	hij, ok := w.(http.Hijacker)
-	if !ok {
-		panic("HTTP Server does not support hijacking")
-	}
-
-	client, _, err := hij.Hijack()
+func handleConnect(w http.ResponseWriter, r *http.Request) {
+	server, err := net.Dial("tcp", r.Host)
 	if err != nil {
+		log.Println(err)
 		return
 	}
 
-	server, err := net.Dial("tcp", host)
-	if err != nil {
-		return
-	}
+	hij, _ := w.(http.Hijacker)
+	client, _, _ := hij.Hijack()
 	client.Write([]byte("HTTP/1.0 200 Connection Established\r\n\r\n"))
 
 	go io.Copy(server, client)
 	io.Copy(client, server)
+}
+
+func (connect *Connect) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Received request %s %s %s\n", r.Method, r.Host, r.RemoteAddr)
+
+	if r.Method == "CONNECT" {
+		handleConnect(w, r)
+	} else {
+		for k, v := range r.Header {
+			fmt.Println(k, v)
+		}
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("This is a http tunnel proxy, only CONNECTED method is allowed"))
+		return
+	}
 }
