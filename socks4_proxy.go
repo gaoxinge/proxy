eb6ba129ed5d485fec74d9aab8af3d3743aa675c
高辛格 <xg.gao@tianrang-inc.com>
2018-10-22 17:49:48 +0800

    socks_proxy

--- socks4_proxy.go
+++ socks4_proxy.go
@@ -30,13 +30,14 @@ func (socks4Proxy *Socks4Proxy) Start(nextAddr string) {
 		client, err := broker.Accept()
 		if err != nil {
 			log.Println(err)
+			continue
 		}
 
-		go serve(client, nextAddr)
+		go serveSocks4(client, nextAddr)
 	}
 }
 
-func serve(client net.Conn, nextAddr string) {
+func serveSocks4(client net.Conn, nextAddr string) {
 	defer client.Close()
 
 	if nextAddr == "" {
@@ -46,36 +47,44 @@ func serve(client net.Conn, nextAddr string) {
 	}
 
 	if nextAddr == "" {
-		reader := bufio.NewReader(client)
-		b, err := reader.Peek(9)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		if b[0] != 0x04 {
-			log.Println(errors.New(fmt.Sprintf("version number %b is not socks4 protocol", b[0])))
-			return
-		}
-
-		switch b[1] {
-		case 0x01:
-			handleSocks4Connect(client, b)
-		case 0x02:
-			log.Println(errors.New(fmt.Sprintf("operation %b is not supproted by socks4proxy", b[1])))
-		default:
-			log.Println(errors.New(fmt.Sprintf("operation %b is not supported by socks4 protocol", b[1])))
-		}
+		handShakeSocks4(client)
 	} else {
-		handleOther(client, nextAddr)
+		handShakeSocks4Other(client, nextAddr)
 	}
 }
 
-func handleSocks4Connect(client net.Conn, b []byte) {
+func handShakeSocks4(client net.Conn) {
+	reader := bufio.NewReader(client)
+	b, err := reader.Peek(9)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	if b[0] != 0x04 {
+		log.Println(errors.New(fmt.Sprintf("version number %b is not socks4 protocol", b[0])))
+		return
+	}
+
 	port := binary.BigEndian.Uint16(b[2:4])
 	host := net.IP(b[4:8])
 	addr := net.JoinHostPort(host.String(), fmt.Sprintf("%d", port))
 
+	switch b[1] {
+	case 0x01:
+		handleSocks4Connect(client, addr, b)
+	case 0x02:
+		log.Println(errors.New(fmt.Sprintf("operation %b is not supproted by socks4proxy", b[1])))
+	default:
+		log.Println(errors.New(fmt.Sprintf("operation %b is not supported by socks4 protocol", b[1])))
+	}
+}
+
+func handShakeSocks4Other(client net.Conn, nextAddr string) {
+	handleSocks4Other(client, nextAddr)
+}
+
+func handleSocks4Connect(client net.Conn, addr string, b []byte) {
 	server, err := net.Dial("tcp", addr)
 	if err != nil {
 		client.Write([]byte{0x00, 0x5b, b[2], b[3], b[4], b[5], b[6], b[7]})
@@ -84,15 +93,15 @@ func handleSocks4Connect(client net.Conn, b []byte) {
 	}
 	client.Write([]byte{0x00, 0x5a, b[2], b[3], b[4], b[5], b[6], b[7]})
 
-	transport(client, server)
+	transportTCP(client, server)
 }
 
-func handleOther(client net.Conn, nextAddr string) {
+func handleSocks4Other(client net.Conn, nextAddr string) {
 	server, err := net.Dial("tcp", nextAddr)
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	transport(client, server)
+	transportTCP(client, server)
 }
